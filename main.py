9877b269b0f0b3d808188fd0c822366eb6eebbe4
Godspeed48 <wialhakimzai@gmail.com>
2025-10-05 00:31:49 +0200

    Count Characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,7 @@ def main():
     book = get_book_text(path)
     num_words = count_words(book)
     print(f"Found {num_words} total words")
-
+    char_dict = count_char(book)
+    print(char_dict)
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_words(book):
     words = book.split()
     return len(words)
+
+def count_char(book):
+    char_dict = {}
+
+    for char in book:
+        char = char.lower()
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    
+    return char_dict
