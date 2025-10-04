34622d4c5546c9d0839ebbceccb46f437f7f95d1
Godspeed48 <wialhakimzai@gmail.com>
2025-10-05 01:52:29 +0200

    Print a Report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_char
+from stats import count_words, count_char, sort_dict, print_alphabetical
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,8 +8,14 @@ def main():
     path = "books/frankenstein.txt"
     book = get_book_text(path)
     num_words = count_words(book)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    char_dict = count_char(book)
-    print(char_dict)
+    print("--------- Character Count -------")
+    char_dict = count_char(book) 
+    list = sort_dict(char_dict)
+    print_alphabetical(list)
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,22 @@ def count_char(book):
             char_dict[char] = 1
     
     return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dict):
+    dicts = []
+    for i in dict:
+        tmp = {"name": i, "num": dict[i]}
+        dicts.append(tmp)
+
+    dicts.sort(reverse=True, key=sort_on)
+    return dicts
+
+def print_alphabetical(list):
+    for d in list:
+        if not d["name"].isalpha():
+            continue
+        else:
+            print(f"{d["name"]}: {d["num"]}")
